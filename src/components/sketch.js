4aee8c7c5fded641a30605894b4bb4812850dffa
Distant22 <sindistant@gmail.com>
2024-02-26 19:14:03 +0800

    Update sketch.js and background

--- src/components/sketch.js
+++ src/components/sketch.js
@@ -1,72 +1,64 @@
 import React from 'react';
 import Sketch from 'react-p5';
-import meme from '../meme.png'
-import bank from '../bank.png'
-import banana from '../banana-peel.png'
-import church from '../church.png'
-import factory from '../factory.png'
-import hamburger from '../hamburger-store.png'
-import park from '../park.png'
-import residence from '../residence.png'
-import school from '../school.png'
-import temple from '../temple.png'
+import background from '../buildings/background.png'
+import bank from '../buildings/bank.png'
+import church from '../buildings/church.png'
+import factory from '../buildings/factory.png'
+import hamburger from '../buildings/hamburger-store.png'
+import park from '../buildings/park.png'
+import residence from '../buildings/residence.png'
+import school from '../buildings/school.png'
+import temple from '../buildings/temple.png'
 
 function P5Sketch({ userInput }) {
 
-    let bankImg, bananaImg, churchImg, factoryImg, hamburgerImg, parkImg, residenceImg, schoolImg, templeImg;
+    let bankImg, churchImg, factoryImg, hamburgerImg, parkImg, residenceImg, schoolImg, templeImg;
     let architecture = [];
     let randomChoice = [];
+    let bg;
+
+    let width = [10,140,270,400,530,660]
+    let height = [10,110,210,310]
+
+    let ranX = []
 
     const setup = (p5, canvasParentRef) => {
-        p5.createCanvas(1000, 500).parent(canvasParentRef);
-        p5.background(255, 255, 255);
-        bankImg = p5.createImg(bank);
+        p5.createCanvas(800, 400).parent(canvasParentRef);
+        bg = p5.loadImage(background)
+        bankImg = p5.loadImage(bank);
         p5.append(architecture, bankImg);
-        churchImg = p5.createImg(church);
+        churchImg = p5.loadImage(church);
         p5.append(architecture, churchImg);
-        bananaImg = p5.createImg(banana);
-        p5.append(architecture, bananaImg);
-        factoryImg = p5.createImg(factory);
+        factoryImg = p5.loadImage(factory);
         p5.append(architecture, factoryImg);
-        hamburgerImg = p5.createImg(hamburger);
+        hamburgerImg = p5.loadImage(hamburger);
         p5.append(architecture, hamburgerImg);
-        parkImg = p5.createImg(park);
+        parkImg = p5.loadImage(park);
         p5.append(architecture, parkImg);
-        residenceImg = p5.createImg(residence);
+        residenceImg = p5.loadImage(residence);
         p5.append(architecture, residenceImg);
-        schoolImg = p5.createImg(school);
+        schoolImg = p5.loadImage(school);
         p5.append(architecture, schoolImg);
-        templeImg = p5.createImg(temple);
+        templeImg = p5.loadImage(temple);
         p5.append(architecture, templeImg);
 
-        for(let x = 0; x < 10; x++){
+        for(let x = 0; x < 6; x++){
+            p5.append(ranX, 10*Math.round(p5.random(0,6)));
             randomChoice[x] = []; // create nested array
-            for(let y = 0; y < 5; y++){
-                randomChoice[x][y] = Math.round(p5.random(0,9));
+            for(let y = 0; y < 4; y++){
+                randomChoice[x][y] = Math.round(p5.random(0,9));                
             } 
         }
-        
+        console.log(ranX)
         console.log(randomChoice);
     }
 
     const draw = (p5) => {
-        // let x = (Math.random() * 200)
-        // let y = (Math.random() * 200)
-        // p5.image(img, x,y,x + 50, y + 50)
-        // p5.image(bankImg, 0, 0, 100, 100);
-        // p5.image(bananaImg, 100, 0, 100, 100);
-        // p5.image(churchImg, 200, 0, 100, 100);
-        // p5.image(factoryImg, 300, 0, 100, 100);
-        // p5.image(hamburgerImg, 400, 0, 100, 100);
-        // p5.image(parkImg, 500, 0, 100, 100);
-        // p5.image(residenceImg, 600, 0, 100, 100);
-        // p5.image(schoolImg, 700, 0, 100, 100);
-        // p5.image(templeImg, 800, 0, 100, 100);
-        // p5.image(templeImg, 900, 0, 100, 100);
-        // p5.image(templeImg, 1000, 0, 100, 100);
-        for(let x = 0; x < 10; x++){
-            for(let y = 0; y < 5; y++){
-                p5.image(architecture[randomChoice[x][y]], 100*x, 100*y, 100, 100);
+        
+        p5.background(bg);
+        for(let x = 0; x < 6; x++){
+            for(let y = 0; y < 4; y++){
+                p5.image(architecture[randomChoice[x][y]], width[x], height[y], 80, 80);
             } 
         }
     }
