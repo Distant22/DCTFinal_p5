b68d33890034a14b723b37cee59ee39bbff60df8
Joe <joechou2002@yahoo.com.tw>
2024-03-19 01:25:25 +0800

    add music functionality for individual

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,15 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Header from '../components/header';
 import Footer from '../components/footer';
 import NameInput from '../components/name';
 import Options from '../components/Options';
 import optionData from '../options/option.json';
 import {ref, uploadBytes, getDownloadURL} from "firebase/storage";
-import { addDoc, collection } from "firebase/firestore";
+import { getDoc, addDoc, collection, doc, updateDoc } from "firebase/firestore";
 import { imgDB, txtDB } from "../firebase";
 import { v4 } from "uuid";
 import localImage from '../buildings/banana-peel.png';
 import ReactLoading from 'react-loading';
+
+import originAudio from '../music/origin.mp3';
+import hopelessAudio from '../music/hopeless.wav';
+import sadAudio from '../music/sad.wav';
+import collapseAudio from '../music/collapse.wav';
+import suicideAudio from '../music/suicide.wav';
+import depressionAudio from '../music/depression.wav';
+import mildAudio from '../music/mild.wav';
+import satisfyAudio from '../music/satisfy.wav';
+import yesAudio from '../music/yes.wav';
+import sunnyAudio from '../music/sunny.wav';
+import cheerAudio from '../music/cheer.wav';
+import happyAudio from '../music/happy.wav';
+
 
 function Main() {
   
@@ -17,6 +31,11 @@ function Main() {
   const [options] = useState(optionData);
   const [result, setResult] = useState(false);
   const [waiting, setWaiting] = useState(true);
+  const audioRef = useRef(null);
+
+  const allAudio = [hopelessAudio, sadAudio, collapseAudio, suicideAudio, depressionAudio, mildAudio, satisfyAudio, yesAudio, sunnyAudio, cheerAudio, happyAudio];
+  const allAudioName = ['hopeless','sad','collapse','suicide','depression','mild','satisfy','yes','sunny','cheer','happy'];
+  let musicSelectedNum;
 
   // 設定初始名稱
   const [user, setUser] = useState({
@@ -68,6 +87,8 @@ function Main() {
               const docRef = await addDoc(collection(txtDB, 'users'), {...user, imgUrl: downloadURL});
               console.log('Document ID:', docRef.id);
               await getImageUrl();
+
+              await updateFirestoreTimestamp();
       
             } catch (error) {
                 console.error('Error adding user data:', error);
@@ -86,6 +107,21 @@ function Main() {
     return () => clearTimeout(waitBeforeRender);
   }, []);
 
+  useEffect(() => {
+    // Automatically play the audio when the component mounts
+    const audioPlayer = audioRef.current;
+    if (audioPlayer) {
+      audioPlayer.play();
+    }
+  
+    // Pause the audio when the component unmounts
+    return () => {
+      if (audioPlayer) {
+        audioPlayer.pause();
+      }
+    };
+  }, []);
+
   const getImageUrl = async () => {
     try {
       const imgRef = ref(imgDB, `Imgs/apple.jpg`);
@@ -96,6 +132,69 @@ function Main() {
     }
   };
 
+  // Determine which audio to play based on the score
+  const getAudioSource = () => {
+    if (user.score >= 1200) {
+      console.log("Playing Audio 'hopeless'");
+      musicSelectedNum = 0;
+      return allAudio[0];
+    } else if (user.score >= 1050) {
+      console.log("Playing Audio 'sad'");
+      musicSelectedNum = 1;
+      return allAudio[1];
+    } else if (user.score >= 900) {
+      console.log("Playing Audio 'collapse'");
+      musicSelectedNum = 2;
+      return allAudio[2];
+    } else if (user.score >= 750) {
+      console.log("Playing Audio 'suicide'");
+      musicSelectedNum = 3;
+      return allAudio[3];
+    } else if (user.score >= 600) {
+      console.log("Playing Audio 'depression'");
+      musicSelectedNum = 4;
+      return allAudio[4];
+    } else if (user.score >= 450) {
+      console.log("Playing Audio 'mild'");
+      musicSelectedNum = 5;
+      return allAudio[5];
+    } else if (user.score >= 300) {
+      console.log("Playing Audio 'satisfy'");
+      musicSelectedNum = 6;
+      return allAudio[6];
+    } else if (user.score >= 150) {
+      console.log("Playing Audio 'yes'");
+      musicSelectedNum = 7;
+      return allAudio[7];
+    } else if (user.score >= 0) {
+      console.log("Playing Audio 'sunny'");
+      musicSelectedNum = 8;
+      return allAudio[8];
+    } else if (user.score >= -150) {
+      console.log("Playing Audio 'cheer'");
+      musicSelectedNum = 9;
+      return allAudio[9];
+    } else if (user.score >= -300) {
+      console.log("Playing Audio 'happy'");
+      musicSelectedNum = 10;
+      return allAudio[10];
+    }
+  };
+
+  // Function to update Firestore document timestamp when music is selected
+  const updateFirestoreTimestamp = async () => {
+    try {
+      const docRef = doc(txtDB, 'music', allAudioName[musicSelectedNum]);
+      const docSnapshot = await getDoc(docRef);
+      const currentTime = docSnapshot.data().time;
+      await updateDoc(docRef, {
+        time: currentTime + 1
+      });
+      console.log('Firestore updated time successfully.');
+    } catch (error) {
+      console.error('Error updating Firestore time:', error);
+    }
+  };
 
   return (
     <div className="flex flex-col h-screen overflow-y-hidden font-serif relative">
@@ -116,7 +215,11 @@ function Main() {
               {result ? 
                 <>
                   <p className="text-sm"> 使用者 {user.name} 的分數是 {user.score}</p>
-                  {imageUrl ? <img src={imageUrl} alt="" /> : <>載入結果中...</>}                
+                  {imageUrl ? <img src={imageUrl} alt="" /> : <>載入結果中...</>}   
+                  <audio ref={audioRef} autoPlay loop>
+                    <source src={getAudioSource()} type="audio/wav" />
+                    Your browser does not support the audio element.
+                  </audio>             
                 </> : 
                 <>  
                   <Options options={options} onChooseOption={handleAddScore} onResult={handleResult} />
