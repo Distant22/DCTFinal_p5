aeda57985de6b6bf3d7ee6a7093313f678c6e51c
Distant22 <sindistant@gmail.com>
2024-04-29 19:27:15 +0800

    Update result image

--- src/components/result_image.js
+++ src/components/result_image.js
@@ -1,10 +1,46 @@
 
-function ResultImage() {
+import React, { useEffect, useState, } from 'react';
+import {ref, getDownloadURL} from "firebase/storage";
+import { imgDB } from "../firebase";
+
+function ResultImage({ userProp, colorProp }) {
+
+  const [imageUrl, setImageUrl] = useState('');
+
+  const colorVariants = {
+    blue: 'bg-[#F2F5F9]',
+    pink: 'bg-[#FFF8F8]',
+    brown: 'bg-[#F8F6F4]',
+    black_and_white: 'bg-[#F5F5F5]',
+    green: 'bg-[#FCFFF6]'
+  }
+
+  const getImageUrl = async () => {
+    try {
+      const imgRef = ref(imgDB, `/result/${userProp.imgType}.png`);
+      const url = await getDownloadURL(imgRef);
+      setImageUrl(url);
+    } catch (error) {
+      console.log("載入圖片發生錯誤：",error)
+    }
+  };
+
+  useEffect(() => {
+    const waitBeforeRender = setTimeout(() => {
+      try {
+        console.log("正在抓取資料：")
+        getImageUrl();
+        console.log("測試 Color : ",colorVariants[colorProp])
+      } catch (error) {
+        console.error('Error adding user data:', error);
+      }
+    }, 100);
+    return () => clearTimeout(waitBeforeRender);
+  }, []);
 
   return (
-    <div className="h-screen w-screen bg-blue-100 flex items-center justify-center">
-        結果圖
-        
+    <div className={`h-dvh w-screen ${colorVariants[colorProp]} flex items-center justify-center`}>
+      <img src={imageUrl} className="object-scale-down h-full w-full" alt="" />     
     </div>
   );
 
--- src/pages/newpage.js
+++ src/pages/newpage.js
@@ -9,6 +9,8 @@ function NewPage() {
 
   const [page, setPage] = useState("init")
 
+  const [color, setColor] = useState('black_and_white');
+
   // 設定初始名稱
   const [user, setUser] = useState({
     name: "",
@@ -20,13 +22,33 @@ function NewPage() {
 
   const handleChangePage = (val) => {
     setPage(val)
-    setUser({...user,
-      name: "",
-      imgType: null,
-      score: 0,
-      uploadTime: null,
-      chosenOption: [],
-    })
+    if(val === "init") {
+      console.log("Reset Everything")
+      setUser({...user,
+        name: "",
+        imgType: null,
+        score: 0,
+        uploadTime: null,
+        chosenOption: [],
+      })
+    } else {
+      if ( user.imgType !== null ){
+        let len = user.imgType.length
+        if ( user.imgType.substring(len - 4, len) === "blue" ) {
+          console.log("Set color to blue")
+          setColor("blue")
+        } else if ( user.imgType.substring(len - 4, len) === "pink") {
+          console.log("Set color to pink")
+          setColor("pink")
+        } else if ( user.imgType.substring(len - 5, len) === "brown"){
+          console.log("Set color to brown")
+          setColor("brown")
+        } else if ( user.imgType.substring(len - 5, len) === "green"){
+          console.log("Set color to green")
+          setColor("green")
+        }
+      }
+    }
   };
 
   const handleUserName = async (val) => {
@@ -60,7 +82,7 @@ function NewPage() {
         { page === "message" ? <Message onNameSubmit={handleUserName} /> : <></>}
         { page === "main" ? <Main nameProp={user.name} setResult={handleResult} /> : <></>}
         { page === "result" ? <Result userProp={user} onRestart={handleChangePage} /> : <></>}
-        { page === "result_image" ? <ResultImage /> : <></>}
+        { page === "result_image" ? <ResultImage userProp={user} colorProp={color} /> : <></>}
     </div>
   );
 }
